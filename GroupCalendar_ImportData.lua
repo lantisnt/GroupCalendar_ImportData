d8b336deffccd43b033a7799035021c0c792ab45
lantisnt <lantisnt@github.com>
2020-02-04 23:18:03 +0100

    Added examples and fixes

--- GroupCalendar_ImportData.lua
+++ GroupCalendar_ImportData.lua
@@ -1,8 +1,8 @@
 -- This file is part of Group Calendar Import Data WoW Classic AddOn.
 
 -- Group Calendar Import Data WoW Classic AddOn is free software: 
--- you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
+-- you can redistribute it and/or modify it
+-- under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 
